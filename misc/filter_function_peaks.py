1a2cb484ed541881866c2698d97364a0901012a6
lmarti <lucas.marti@fau.de>
2024-10-22 15:01:40 +0200

    moved runs to qutlet runs

--- misc/filter_function_peaks.py
+++
@@ -1,8 +0,0 @@
-from fermionic_cooling.filter_functions import get_fourier_gaps_filter_function
-from qutlet.models import FermiHubbardModel
-
-
-model = FermiHubbardModel(
-    lattice_dimensions=(2, 2), tunneling=1, coulomb=6, n_electrons="hf"
-)
-energies, states = model.spectrum
